cba48eb8d2b695fcd25d1e53bce22e3168080bfa
UsaMomokawa <42172002+UsaMomokawa@users.noreply.github.com>
2023-11-20 19:02:22 +0900

    fix path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Noto_Sans_JP } from "next/font/google"
 import { twMerge } from "tailwind-merge"
-import "./globals.css"
+import "@/styles/globals.css"
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
