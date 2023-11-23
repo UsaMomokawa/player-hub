4c35c7bd93f42c5bd380cfe887435d5578ae48b5
UsaMomokawa <42172002+UsaMomokawa@users.noreply.github.com>
2023-11-23 20:11:14 +0900

    fix

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -2,11 +2,16 @@ import type { Meta, StoryObj } from "@storybook/react"
 import { Header } from "@/components/Header"
 
 const meta = {
-  title: "Header/Header",
+  title: "Header",
   component: Header,
   tags: ["autodocs"],
   parameters: {
     layout: "fullscreen",
+  },
+  argTypes: {
+    backButton: { control: "boolean" },
+    onMenuClick: { action: "menuButtonClicked" },
+    onBackClick: { action: "backButtonClicked" },
   },
 } satisfies Meta<typeof Header>
 
