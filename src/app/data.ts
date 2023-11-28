5bc4f5a12c2d4ad2fe919e5b2663d9d936772b88
UsaMomokawa <42172002+UsaMomokawa@users.noreply.github.com>
2023-11-28 13:53:56 +0900

    fix function

---
+++ src/app/data.ts
@@ -0,0 +1,43 @@
+interface Character {
+  id: string,
+  name: string,
+  furigana?: string,
+  age?: number,
+  birthday?: {
+    month: number,
+    day: number,
+  },
+  height?: number,
+  memo?: string,
+  scenarios?: string[],
+}
+
+function getBaseUrl(): string {
+  const vc = process.env.VERCEL_URL;
+  if (vc) {
+    return `https://${vc}`;
+  } else {
+    return 'http://localhost:3000';
+  }
+}
+
+export async function fetchCharacters(): Promise<Character[]> {
+  try {
+    const baseUrl = getBaseUrl();
+    const res = await fetch(`${baseUrl}/characters.json`);
+    const data = await res.json();
+    return data.characters;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+export async function fetchCharacterById(id: string): Promise<Character | undefined> {
+  const characters = await fetchCharacters();
+  return characters.find((c) => c.id === id);
+}
+
+export function getCharacterImage(id: string): string {
+  return `/portraits/${id}.png`;
+}
