6e724ef130ff2df32280ee26376a319b7f95211f
UsaMomokawa <42172002+UsaMomokawa@users.noreply.github.com>
2023-11-28 19:27:12 +0900

    fix card size

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,7 +21,7 @@ export const Card = ({ href, imgSrc = "", label, ...props }: CardProps) => {
     >
       <div
         className={twMerge(
-          "relative overflow-hidden h-36 w-36 rounded-t-lg",
+          "relative overflow-hidden h-36 w-36 xl:h-44 xl:w-44 rounded-t-lg",
           "border-2 border-b-0 border-stone-200",
           "bg-warm-50",
         )}
@@ -39,7 +39,7 @@ export const Card = ({ href, imgSrc = "", label, ...props }: CardProps) => {
       </div>
       <p
         className={twMerge(
-          "inline-block text-center py-3 w-36 rounded-b-lg",
+          "inline-block text-center py-3 w-36 xl:w-44 rounded-b-lg",
           "border-2 border-t-0 border-stone-200",
           "text-stone-800",
           "bg-white",
