18b30a88e8ef2eb9ac561f71979815dc1f83b29a
UsaMomokawa <42172002+UsaMomokawa@users.noreply.github.com>
2023-11-29 14:40:07 +0900

    height と width は固定しない

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,12 +21,12 @@ export const Card = ({ href, imgSrc = "", label, ...props }: CardProps) => {
     >
       <div
         className={twMerge(
-          "relative overflow-hidden h-48 w-36 xl:h-56 xl:w-44 rounded-lg",
+          "relative overflow-hidden rounded-lg",
           "border-2 border-b-0 border-stone-200",
           "bg-warm-50",
         )}
       >
-        <div className="h-full w-full absolute"></div>
+        <div className="absolute h-full w-full"></div>
         <Image
           src={imgSrc}
           width={250}
@@ -39,7 +39,7 @@ export const Card = ({ href, imgSrc = "", label, ...props }: CardProps) => {
       </div>
       <span
         className={twMerge(
-          "absolute text-center py-3 w-36 xl:w-44 rounded-b-lg",
+          "absolute text-center py-3 w-full rounded-b-lg",
           "border-2 border-t-0 border-stone-200",
           "text-stone-800",
           "bg-white",
