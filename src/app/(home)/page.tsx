82a0b8b511088d5b275ae58cad8054f157b9ee51
UsaMomokawa <42172002+UsaMomokawa@users.noreply.github.com>
2023-11-30 18:28:28 +0900

    個別に当てる

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
   return (
     <>
       <div className="xl:checker-pattern">
-        <Container>
+        <Container className="max-w-screen-xl">
           <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 xl:gap-12 py-10 pt-12 xl:py-16 xl:pt-16">
             {characters &&
               characters.map((character) => (
