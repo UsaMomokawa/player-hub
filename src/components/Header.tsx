9269c95d7911aaaa8cc4b61f75579d0aea382f43
Momoka Komatsu <42172002+UsaMomokawa@users.noreply.github.com>
2023-11-30 20:41:11 +0900

    Revert "幅調整"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export const Header = ({
   return (
     <header>
       <div className="sticky w-full">
-        <Container className="max-w-screen-xl">
+        <Container>
           <div
             className={twMerge(
               "flex flex-row py-6 items-center justify-between",
